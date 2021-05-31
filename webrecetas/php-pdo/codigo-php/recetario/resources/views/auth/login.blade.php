211c2b1f8130f07e9568babe7c81a66435635613
rbozaalumno <rboza.alumno@iesrafaelalberti.es>
2021-05-31 12:14:00 +0200

    login/register

--- webrecetas/php-pdo/codigo-php/recetario/resources/views/auth/login.blade.php
+++ webrecetas/php-pdo/codigo-php/recetario/resources/views/auth/login.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="col-md-8 mt-5">
+            <div class="card" style="border: 5px solid #BF4D67;border-radius: 0px 5px 5px 0px;">
+                <div class="card-header" style="border-radius:0px;border-bottom: 2px solid #BF4D67;font-family:Pacifico;font-size:25px;background-color:#D88E8E">{{ __('Login') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="background-color:#AEE5D8;">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -15,7 +15,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror category-select" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror category-select" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
--- webrecetas/php-pdo/codigo-php/recetario/resources/views/auth/register.blade.php
+++ webrecetas/php-pdo/codigo-php/recetario/resources/views/auth/register.blade.php
@@ -2,20 +2,25 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-6 nomarpad">
+            <div class="card" style="height:100%;position:none;border: 5px solid #BF4D67; border-right:0px;border-radius: 5px 0px 0px 5px;">
+                <div class="card-body nomarpad" style="background: no-repeat;text-align:center;background-image: url(../images/cutlery.jpg);background-size: 100%;background-origin: content-box;background-position: center;">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 nomarpad">
+            <div class="card" style="border: 5px solid #BF4D67;border-radius: 0px 5px 5px 0px;">
+                <div class="card-header" style="border-radius:0px;border-bottom: 2px solid #BF4D67;font-family:Pacifico;font-size:25px;background-color:#D88E8E">{{ __('Registro') }}</div>
+                <div class="card-body" style="background-color:#AEE5D8;">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right" style="font-family:Lato">{{ __('Nombre') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror category-select" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +31,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right" style="font-family:Lato">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror category-select" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -40,10 +45,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right" style="font-family:Lato">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror category-select" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -54,10 +59,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right" style="font-family:Lato">{{ __('Confirmar Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control category-select" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
